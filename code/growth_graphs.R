853aa9e6b8463bdc8d7698431979910e4917f6a1
Max <max.w.callaghan@gmail.com>
2017-06-13 16:28:13 +0200

    growth plots

--- code/growth_graphs.R
+++ code/growth_graphs.R
@@ -33,6 +33,67 @@ q <- paste0('SELECT "scoping_doc"."UT", "scoping_doc"."PY", "scoping_wc"."oecd",
 
 alldocs <- data.frame(dbGetQuery(con, q)) 
 
-alldocs <- filter(alldocs,PY>1985)
+alldocs <- filter(alldocs,PY>1985,PY<2017)
 
+pn <- scimetrix::paperNumbers(alldocs,"oecd",graph=T) + scale_x_continuous(breaks = seq(1985,2020,by=5))
+
+ggsave("plots/paper_numbers.png")
+
+
+
+########################################################
+
+papers <- alldocs %>%
+  group_by(PY) %>%
+  summarise(n=n())
+
+#cut papers by assessment period
+papers$AP <- cut(papers$PY,
+                 c(0,1985,1990.1,1995.1,2001.1,2007.1,2013.1,Inf),
+                 c(NA,"AR1","AR2","AR3","AR4","AR5","AR6")
+)
+
+apCounts <- papers %>%
+  group_by(AP) %>%
+  summarise(
+    total = formatC(sum(n),format="d", big.mark=',',preserve.width="none"),
+    midY = median(PY),
+    maxV = max(n),
+    n = sum(n)
+  ) %>%
+  ungroup() %>%
+  filter(AP %in% c("AR1","AR2","AR3","AR4","AR5","AR6")) %>%
+  mutate(
+    total = paste0("[",total,"]")
+  )
+
+####################
+### Figure 1: Growth
+growth <- ggplot() +
+  geom_bar( # Bars for each year, colour coded by AP
+    data = filter(papers,PY > 1985 & PY < 2017),
+    aes(PY,n,fill=AP),
+    stat="identity",
+    colour="grey22"
+  ) +
+  geom_text( # Labels
+    data=apCounts,
+    aes(label=total,x=midY,y=maxV+100),
+    hjust = 0.5
+  ) +
+  labs(x="Year",y="Number of Publications") +
+  theme_classic() +
+  theme(
+    text=element_text(size=12),
+    legend.position=c(0.1,0.9),
+    legend.justification=c(0,1),
+    legend.direction="horizontal",
+    panel.grid.major.y=element_line(size=0.2,colour="grey22"),
+    panel.border=element_rect(size=0.2,colour="grey22",fill=NA)
+  ) +
+  scale_fill_manual(values=APscale[1:6],name="Assessment Period") +
+  scale_x_continuous(breaks = seq(1985,2015,by=5))
+
+growth
+ggsave("plots/total_growth.png")
 
